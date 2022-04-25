6278c278d646566ae875ac6eef53ec7ec0cc3d3e
Sebastian Yde Madsen <75695915+seba2390@users.noreply.github.com>
2022-04-26 00:01:38 +0200

    Add files via upload

--- Optimization/Artificial bee colony/my_class.py
+++ Optimization/Artificial bee colony/my_class.py
@@ -4,6 +4,8 @@ from tqdm import tqdm
 
 
 class ArtificialBeeColony:
+    """A class representation of an Artificial Bee Colony (ABC)"""
+
     def __init__(self, func, Dimensions_bounds, Nr_bees, Nr_iterations, 
                                    Max_trial_count, Save_history = True):
 
@@ -16,25 +18,64 @@ class ArtificialBeeColony:
         self._Save_history      = Save_history
         self._Max_trial_count   = Max_trial_count
 
-        self.food_sources  = self.initializeFoodSources()       # Nr. of foud sorces equal nr. of bees
-        self.nectars       = np.array([self.getNectar(food_source) for food_source in self.food_sources])
-        self.trial_counts  = np.zeros(len(self.food_sources))   # Number of times a food source has been visited
-        self.BestFoodSoure = self.getBestFoodSource()            # Returns food source with highest fitness
+        self.dimensionBoundsAssertion()  # Checking the given bounds for the dimensions of problem
+
+        self.food_sources    = self.initializeFoodSources()   # Nr. of foud sorces equal nr. of bees
+        self.nectars         = self.initializeNectars()       # Nr. of nectars equal nr. of food sources
+        self.trial_counts    = self.initializeTrialCounts()   # List of nr. of times a food source has been visited
+
+        self.BestFoodSoure   = self.getBestFoodSource()       # Returns food source with highest fitness
     
         if self._Save_history: 
             self.food_sources_history = [self.food_sources]
 
     def calculateFitness(self,food_source):
+        """ Function for calculating the fitness of some food source according to
+            the chosen function.
+            
+        Parameters:
+        -----------
+            food_source: np.ndarray of shape (self._Nr_dimensions,) containing floats
+
+        Returns:
+        --------
+            fitness: float"""
         func_eval = self._f(food_source)
+        fitness   = None
         if func_eval >= 0:
-            return 1. / (1. + func_eval)
+            fitness = 1. / (1. + func_eval)
         else:
-            return 1 + np.abs(func_eval)   
+            fitness = 1 + np.abs(func_eval)  
+        return fitness 
 
     def getNectar(self,food_source):
-        return self.calculateFitness(food_source)
+        """A syntactically pleasing wrapper for 'calculateFitness'. 
+
+        Parameters:
+        -----------
+            food_source: np.ndarray of shape (self._Nr_dimensions,) containing floats
+
+        Returns:
+        --------
+            nectar: float"""
+        nectar = self.calculateFitness(food_source)
+        return nectar
+    
+    def initializeTrialCounts(self):
+        trial_counts = np.zeros(len(self.food_sources))
+        return trial_counts
+    
+    def initializeNectars(self):
+        nectars = np.array([self.getNectar(food_source) for food_source in self.food_sources])
+        return nectars
 
     def initializeFoodSources(self):
+        """A function for creating the initial food sources. 
+
+        Returns:
+        --------
+            food_sources: np.ndarray of shape (self._Nr_bees, self._Nr_dimensions)
+        """
         food_sources = np.empty(shape=(self._Nr_bees, self._Nr_dimensions), dtype=object)
         for food_source in range(self._Nr_bees):        
             for dimension in range(self._Nr_dimensions):
@@ -44,8 +85,15 @@ class ArtificialBeeColony:
                                                                                     * (dth_upper - dth_lower)
         return food_sources 
     
-
     def sendEmployedBees(self):
+        """ A function representation of sending out the Employed bees 
+            and evaluating food sources
+        
+        Setting:
+        --------
+            self.food_sources, self.nectars, self.trial_counts
+        """
+
         candidate_food_sources = np.empty(shape = (self._Nr_bees, self._Nr_dimensions), dtype=object)
         for i in range(self._Nr_bees):
             indices = np.concatenate([np.arange(start = 0, stop = i),\
@@ -65,6 +113,13 @@ class ArtificialBeeColony:
             else: self.trial_counts[food_source] += 1
 
     def sendOnlookerBees(self):
+        """ A function representation of sending out the Onlooker bees 
+            and evaluating food sources
+        
+        Setting:
+        --------
+            self.food_sources, self.nectars, self.trial_counts
+        """
         candidate_food_sources, candidate_nectars = self.FitnessProportionateSelection()
         for food_source in range(len(self.food_sources)):
             if candidate_nectars[food_source] > self.nectars[food_source]:
@@ -74,6 +129,13 @@ class ArtificialBeeColony:
             else: self.trial_counts[food_source] += 1
 
     def sendScoutBees(self):
+        """ A function representation of sending out the Scout bees 
+            and evaluating food sources
+        
+        Setting:
+        --------
+            self.food_sources, self.nectars, self.trial_counts
+        """
         for food_source in range(len(self.food_sources)):
             if self.trial_counts[food_source] >= self._Max_trial_count:
                 ## Resetting
@@ -89,7 +151,13 @@ class ArtificialBeeColony:
 
     def FitnessProportionateSelection(self):
         """Applying 'Fitness Proportionate Selection' also known
-           as 'Roulette Wheel Selection' to determine """
+           as 'Roulette Wheel Selection' to determine
+        
+        Returns:
+        --------
+            candidate_food_sources: np.ndarray of shape (self._Nr_bees, self._Nr_dimensions)
+            candidate_nectars: np.ndarray of shape (self._Nr_bees,)
+        """
         probabilities = np.array([self.nectars[i] / np.sum(self.nectars) for i \
                                                     in range(len(self.nectars))])
         accumulated_probabilities = np.array([np.sum(probabilities[:i+1]) for i \
@@ -107,20 +175,45 @@ class ArtificialBeeColony:
         return candidate_food_sources, candidate_nectars
         
     def getBestFoodSource(self):
+        """ A function that determines the food source with the
+            highest amount of nectar.
+            
+        Returns:
+        --------
+            bestFoodSource: np.ndarray of shape (self._Nr_dimensions,) containing floats
+             """
         bestNectar, bestFoodSource = 0, None
         for i, nectar in enumerate(self.nectars):
             if nectar > bestNectar: 
                 bestNectar = nectar
                 bestFoodSource = self.food_sources[i]
         return bestFoodSource
+
+    def getBestFoodSources(self):
+        """ A function that determines the chosen nr. of food source with the
+            highest amount of nectar.
+            
+        Returns:
+        --------
+        bestFoodSource: np.ndarray of shape (self._Nr_dimensions,) containing floats
+            """
+        sorting_indexes     = np.flip(np.argsort(self.nectars, kind= "heap"))
+        sorted_food_sources = self.food_sources[sorting_indexes]
+        return sorted_food_sources[:self._Nr_solutions]
     
+    def dimensionBoundsAssertion(self):
+        """ A helper function for asserting the given bounds."""
+        for d, dimension in enumerate(self._Dimensions_bounds):
+            assert dimension[0] < dimension[1], f'{d+1} th dimension has lower >= upper.'
+
     def simulate(self):
+        """ A function for simulating 'iteration' nr. of cycles. """
         print("-"*10+" Performing cycles "+10*"-")
         for iteration in tqdm(range(self._Nr_iterations)):
             self.sendEmployedBees()
             self.sendOnlookerBees()
             self.sendScoutBees()
-            self.BestFoodSoure = self.getBestFoodSource()
+            self.BestFoodSoure   = self.getBestFoodSource()
             if self._Save_history: 
                 self.food_sources_history.append(deepcopy(self.food_sources))
         
